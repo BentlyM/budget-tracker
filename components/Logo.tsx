54e381c317622f867b7cb488466b43c751b16e24
BentlyM <metayerbently@gmail.com>
2024-10-08 10:26:17 -0400

    added mobile navigation

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -12,4 +12,14 @@ const Logo = () => {
   )
 }
 
+
+export const LogoMobile = () => {
+    return (
+      <a href='/' className='flex items-center gap-2'>
+          <p className='bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3x1 font-bold leading-tight tracking tighter text-transparent'>
+              BudgetTracker
+          </p>
+      </a>
+    )
+}
 export default Logo
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,21 @@
 'use client';
 
-import React from 'react';
-import Logo from './Logo';
+import React, { useState } from 'react';
+import Logo, { LogoMobile } from './Logo';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
 import { UserButton } from '@clerk/nextjs';
 import { ThemeSwitcherBtn } from './ThemeSwitcherBtn';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { Menu } from 'lucide-react';
 
 const Navbar = () => {
   return (
     <>
-      <DesktopNavBar />
+      <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
 };
@@ -23,7 +26,45 @@ const items = [
   { label: 'Manage', link: '/manage' },
 ];
 
-function DesktopNavBar() {
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant={'ghost'} size={'icon'}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]" side={'left'}>
+            <Logo />
+            <div className="flex flex-col gap-1 pt-4">
+              {items.map((item) => (
+                <NavbarItem
+                  key={item.label}
+                  link={item.link}
+                  label={item.label}
+                  clickCallback={()=> setIsOpen((prev) => !prev)}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className='flex h-[80px] min-h-[60px] items-center gap-x-4'>
+            <LogoMobile />
+        </div>
+        <div className='flex items center gap-2'>
+            <ThemeSwitcherBtn />
+            <UserButton />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function DesktopNavbar() {
   return (
     <div className="hidden border-separate border-b bg-background md:flex justify-center">
       <nav className="container flex items-center justify-between px-8">
@@ -39,7 +80,7 @@ function DesktopNavBar() {
             ))}
           </div>
         </div>
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           <ThemeSwitcherBtn />
           <UserButton />
         </div>
@@ -48,7 +89,7 @@ function DesktopNavBar() {
   );
 }
 
-function NavbarItem({ link, label }: { link: string; label: string }) {
+function NavbarItem({ link, label, clickCallback }: { link: string; label: string; clickCallback?: VoidFunction}) {
   const pathname = usePathname();
   const isActive = pathname === link;
 
@@ -64,6 +105,9 @@ function NavbarItem({ link, label }: { link: string; label: string }) {
             'flex justify-center text-lg text-muted-foreground hover:text-foreground',
             isActive && 'text-foreground'
           )}
+          onClick={() => {
+            if(clickCallback) clickCallback();
+          }}
         >
           {label}
         </Link>
